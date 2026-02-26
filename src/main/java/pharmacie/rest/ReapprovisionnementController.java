1bb98a1424626c89a9d45ae32ac20143a9b39110
Baptiste32270 <baptiste.faugeres@sfr.fr>
2026-02-26 11:17:34 +0100

    modif pour frontend

--- src/main/java/pharmacie/rest/ReapprovisionnementController.java
+++ src/main/java/pharmacie/rest/ReapprovisionnementController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pharmacie.service.ReapprovisionnementService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/reapprovisionnement")
 public class ReapprovisionnementController {
 
